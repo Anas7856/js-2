4cbb5aa107cdc64e2f185bb6408277418a905113
Anas zahoor <ngsanas04@gmail.com>
2024-09-16 19:20:34 +0500

    js 2

---
+++ scpt.js
@@ -0,0 +1,38 @@
+// table
+// var userinput = prompt("enter the tabe number:");
+// var table = parseInt(userinput);
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i + "x" + table + "=" + i * table);
+// }
+// even number 1 to 100 sum
+var sum = 0;
+
+// for (var i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+
+console.log(sum);
+
+// Factorial calculation
+// var promptVal = prompt(
+//   "Enter the number you want to calculate the factorial of"
+// );
+
+// var num = parseInt(promptVal);
+
+var fact = 1;
+// 5
+// for (var i = num; i >= 1; i--) {
+//   fact = fact * i;
+// }
+
+// console.log("Factorial of " + num + " = " + fact);
+
+var n = 10;
+//  %
+
+if (n % 2 === 0) {
+  console.log(n + " is even");
+} else {
+  console.log(n + " is odd");
+}
